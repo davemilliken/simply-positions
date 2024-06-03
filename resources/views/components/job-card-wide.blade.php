67e88d150c784978f7c4a678fe64c0374349749b
davemilliken <davemilliken0@gmail.com>
2024-06-03 18:49:57 +0100

    LaracastsFinalProjectEndVideo2

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<x-panel class="flex gap-x-6 ">
+    <div>
+        <x-employer-logo :width="100" />
+    </div>
+
+    <div class="flex-1 flex flex-col group-hover:text-blue-600 transistion-colors duration-300">
+        <a href="#" class="self-start text-sm text-gray-400">
+            Laracasts
+        </a>
+
+        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From £60,000</p>
+    </div>
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
